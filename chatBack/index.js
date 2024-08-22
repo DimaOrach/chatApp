4925921d8bbe1febcde6d6fdefb197dc9d0a1e90
DimaOrach1 <DimaOrach1@example.com>
2024-08-22 22:36:22 +0300

    Database connection

--- chatBack/index.js
+++ chatBack/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import Connect from './db/connection.js';
 
 const app = express();
+app.use(express());
+app.use(cors());
 
 async function start() {
-
+    Connect();
     const PORT = process.env.PORT || 5000;
     
     await app.listen(PORT, () => console.log(`SERVER IS WORKING ON PORT ${PORT}`));
